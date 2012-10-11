266b308687b2ce8f1b7b268c2144212f137e001c
Valdas <valdas.kriau@gmail.com>
2012-10-11 11:24:45 +0100

    Changed DB

--- BuyLocalBonds/BackEnd/SQLBean.cs
+++ BuyLocalBonds/BackEnd/SQLBean.cs
@@ -14,13 +14,15 @@ namespace BuyLocalBonds
 
         public SQLBean()
         {
-            conn = new SqlConnection("Server=.;Database=BLBData;Integrated Security=SSPI;");
+                        
         }
         public DataSet GetBonds(double low, double high)
         {
+            conn = new SqlConnection("Server=.;Database=BLBData;Integrated Security=SSPI;");
             DataSet ds = new DataSet();
             SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Bonds WHERE Price between " + low + " and " + high, conn);
             da.Fill(ds, "Bonds");
+            conn.Close();
             return ds;
         }
     }
