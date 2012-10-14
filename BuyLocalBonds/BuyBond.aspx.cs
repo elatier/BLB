8248080e39ad086c78d2ef0be076b52dcfdcfe7e
Valdas <valdas.kriau@gmail.com>
2012-10-14 23:48:07 +0100

    Added bond select function

--- BuyLocalBonds/BuyBond.aspx.cs
+++ BuyLocalBonds/BuyBond.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using BuyLocalBonds.BackEnd;
 
 namespace BuyLocalBonds
 {
@@ -11,7 +13,27 @@ namespace BuyLocalBonds
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Response.BufferOutput = true;
 
+            String cusip = Request.QueryString["CUSIP"];
+            if (!String.IsNullOrWhiteSpace(cusip))
+            {
+                BEnd bend = new BEnd();
+                    
+                DataSet ds = bend.SelectBond(cusip);
+                GridView1.DataSource = ds.Tables[0];
+                GridView1.DataBind();
+            }
+            else
+            {
+                //Server.Transfer("Default.aspx", true);
+                Response.Redirect("~/Default.aspx", true);
+            }
+        }
+
+        protected void Back_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Default.aspx", true);
         }
     }
 }
