9a3aeac93d11c14a3512936d7291880422068eeb
Valdas <valdas.kriau@gmail.com>
2012-10-17 09:57:00 +0100

    Added testFile for bonds.

---
+++ TestBLB/TestBond.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using BuyLocalBonds.BackEnd;
+
+namespace TestBLB
+{
+
+    [TestFixture]
+    public class TestBond
+    {
+        private Bond b;
+
+        [TestFixtureSetUp]
+        public static void overallInit()
+        {
+            Console.WriteLine("could do common initialization here");
+        }
+
+        [TestFixtureTearDown]
+        public static void overallTeardown()
+        {
+            Console.WriteLine("could do common cleanup here");
+        }
+
+        [SetUp]
+        public void setUp()
+        {
+            b = new Bond();
+        }
+
+        [TearDown]
+        public void tearDown()
+        {
+            b = null;
+        }
+
+        [Test]
+        public void testNotNull()
+        {
+            Assert.NotNull(b);
+        }
+
+        [Test]
+        public void testConstructor()
+        {
+           // b = new Bond("123", "name1", 123.123);
+            Assert.AreEqual("123", b.Cusip);
+            Assert.AreEqual("name1", b.Name);
+           // Assert.AreEqual(123.123, b.Price);
+        }
+
+    }
+}
